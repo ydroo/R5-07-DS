7927c362559c23a1c5af4bad28f6074918647a00
Trystan B. <130407095+ydroo@users.noreply.github.com>
2024-11-20 14:56:35 +0100

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -2,9 +2,9 @@ public class Q4 {
 	public static void main (String[] a){
 		int x = Integer.parseInt(a[0]);
 		String s = "";
-		for (int i=0; i<x; i++)
+		for (int i=1; i<=x; i++)
 		{
-			for (int j= 0; j<i; j++)
+			for (int j=0; j<=i; j++)
 				s+= "*";
 			s+="\n";
 		}
